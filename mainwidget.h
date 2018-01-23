40a68084565ef1396ec70e1a95e583cae779a8da
JambonBeurreMan <jambonbeurreman@gmail.com>
2018-01-24 00:22:23 +0100

    UI for Android

--- mainwidget.h
+++ mainwidget.h
@@ -5,6 +5,7 @@
 
 #include <QMessageBox>
 #include <QVariant>
+#include <QScreen>
 
 #include "touchscreen.h"
 #include "configwindow.h"
@@ -23,10 +24,15 @@ private:
                  *clearImageButton, *configGamepadButton;
     QLineEdit    *addrLineEdit;
     QSlider      *touchOpacitySlider;
+    QLabel       *creatorLabel, *titleLabel, *instructionsLabel;
     ConfigWindow *settingsConfig;
+    QPixmap      *logo, *title;
+    QScreen      *screen;
+    QFrame       *lineA, *lineB, *lineC;
 
 public:
     TouchScreen *touchScreen;
+    QString version = QCoreApplication::applicationVersion();
 
     Widget(QWidget *parent = nullptr) : QWidget(parent)
     {
@@ -34,22 +40,69 @@ public:
 
         addrLineEdit = new QLineEdit(this);
         addrLineEdit->setClearButtonEnabled(true);
+        addrLineEdit->setInputMethodHints(Qt::ImhPreferNumbers);
 
         formLayout = new QFormLayout();
-        formLayout->addRow(tr("IP &address"), addrLineEdit);
+        formLayout->addRow(tr("IP address 📡"), addrLineEdit);
 
         touchOpacitySlider = new QSlider(Qt::Horizontal);
         touchOpacitySlider->setRange(0, 10);
         touchOpacitySlider->setValue(10);
         touchOpacitySlider->setTickInterval(1);
-        formLayout->addRow(tr("TS &Opacity"), touchOpacitySlider);
+        //formLayout->addRow(tr("TS Opacity"), touchOpacitySlider);
 
-        homeButton = new QPushButton(tr("&Home"), this);
-        powerButton = new QPushButton(tr("&Power"), this);
-        longPowerButton = new QPushButton(tr("Power (&long)"), this);
-        settingsConfigButton = new QPushButton(tr("&Settings"), this);
-        clearImageButton = new QPushButton(tr("&Clear Image"), this);
-        configGamepadButton = new QPushButton(tr("&Configure Custom Gamepad"));
+        homeButton = new QPushButton(tr("Home 🏠"), this);
+        powerButton = new QPushButton(tr("Power ⚫"), this);
+        longPowerButton = new QPushButton(tr("Power (long) ⚫⚫⚫"), this);
+        settingsConfigButton = new QPushButton(tr("Settings ⚙️"), this);
+        //clearImageButton = new QPushButton(tr("Clear Image"), this);
+        configGamepadButton = new QPushButton(tr("Configure Custom Gamepad 🎮"));
+
+        QScreen *screen = QApplication::screens().at(0);
+        titleLabel = new  QLabel;
+        titleLabel->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Maximum);
+        QPixmap title(":/myressources/logo/InputRedirectionClient.png");
+        titleLabel->setPixmap(title.scaledToWidth(screen->availableGeometry().width()-20));
+        titleLabel->setAlignment(Qt::AlignCenter);
+
+        lineA = new QFrame;
+        lineA->setFrameShape(QFrame::HLine);
+        lineA->setFrameShadow(QFrame::Sunken);
+
+        lineB = new QFrame;
+        lineB->setFrameShape(QFrame::HLine);
+        lineB->setFrameShadow(QFrame::Sunken);
+
+        lineC = new QFrame;
+        lineC->setFrameShape(QFrame::HLine);
+        lineC->setFrameShadow(QFrame::Sunken);
+
+        instructionsLabel = new QLabel;
+        instructionsLabel->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+        instructionsLabel->setAlignment(Qt::AlignLeft);
+        instructionsLabel->setText("<html><b>Instructions:</b>"
+                                   "<br>1. Start InputRedirection on your 3DS via Rosalina menu (Press L+⬇️+Select)."
+                                   "<br>2. Connect your Gamepad to your Android device.</br>"
+                                   "<br>3. Enter the IP address displayed on your 3DS in the IRC application.</br>"
+                                   "<br>ℹ️ Work via Wi-Fi or via Mobile Data if both devices share the same network.</br></html>");
+        instructionsLabel->setTextFormat(Qt::RichText);
+        instructionsLabel->setWordWrap(true);
+
+        creatorLabel = new QLabel;
+        creatorLabel->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+        creatorLabel->setAlignment(Qt::AlignCenter);
+        creatorLabel->setText("<html>Created by <a href=\"https://github.com/TuxSH/\">TuxSH</a>"
+                              "<br>Based on <a href=\"https://github.com/mastermune/\">mastermune</a> & <a href=\"https://github.com/gbrown5/\">gbrown5</a>'s forks"
+                              "<br>Version "+version+" for Android by JambonBeurreMan</html>");
+        creatorLabel->setTextFormat(Qt::RichText);
+        creatorLabel->setTextInteractionFlags(Qt::TextBrowserInteraction);
+        creatorLabel->setOpenExternalLinks(true);
+
+        if(screen->availableGeometry().height()<=800)
+        {
+            instructionsLabel->setStyleSheet("font: 10pt;");
+            creatorLabel->setStyleSheet("font: 10pt");
+        }
 
         setContextMenuPolicy(Qt::CustomContextMenu);
 
@@ -61,13 +114,26 @@ public:
              configGamepadButton->setVisible(false);
          }
 
+        layout->setContentsMargins(10,10,10,0);
+        layout->addWidget(titleLabel);
+        layout->addSpacing(10);
         layout->addLayout(formLayout);
+        layout->addWidget(lineA);
         layout->addWidget(homeButton);
+        if(screen->availableGeometry().height()>=1280)
+        {
+            layout->addSpacing(50);
+        }
         layout->addWidget(powerButton);
         layout->addWidget(longPowerButton);
         layout->addWidget(configGamepadButton);
         layout->addWidget(settingsConfigButton);
-        layout->addWidget(clearImageButton);
+        //layout->addWidget(clearImageButton);
+        layout->addWidget(lineB);
+        layout->addWidget(instructionsLabel);
+        layout->addStretch(1);
+        layout->addWidget(lineC);
+        layout->addWidget(creatorLabel);
 
         gpConfigurator = new GamepadConfigurator();
 
@@ -165,20 +231,20 @@ public:
             } else settingsConfig->hide();
         });
 
-        connect(clearImageButton, &QPushButton::released, this,
+        /*connect(clearImageButton, &QPushButton::released, this,
                 [this](void)
         {
            touchScreen->clearImage();
-        });
+        });*/
 
-        connect(touchOpacitySlider, &QSlider::valueChanged, this,
+        /*connect(touchOpacitySlider, &QSlider::valueChanged, this,
                 [this](int value)
         {
             touchScreen->setWindowOpacity(value / 10.0);
             touchScreen->update();
-        });
+        });*/
 
-        touchScreen = new TouchScreen(nullptr);
+        //touchScreen = new TouchScreen(nullptr);
         settingsConfig = new ConfigWindow(nullptr, touchScreen);
         this->setWindowTitle(tr("InputRedirectionClient-Qt"));
 
@@ -188,8 +254,8 @@ public:
     void show(void)
     {
         QWidget::show();
-        touchScreen->move(this->x() + this->width() + 5,this->y());
-        touchScreen->show();
+        //touchScreen->move(this->x() + this->width() + 5,this->y());
+        //touchScreen->show();
         settingsConfig->hide();
     }
 
@@ -209,22 +275,22 @@ public:
          {
              settings.remove(tr("tsShortcut%1").arg(i));
          }
-        touchScreen->close();
+        //touchScreen->close();
         settingsConfig->close();
 
-        touchScreen->setTouchScreenPressed(false);
-        delete touchScreen;
+        //touchScreen->setTouchScreenPressed(false);
+        //delete touchScreen;
         ev->accept();
     }
 
     //Move touchscreen window with main window if moved
     void moveEvent(QMoveEvent *event)
     {
-        touchScreen->move(touchScreen->pos() + (event->pos() - event->oldPos()));
+        //touchScreen->move(touchScreen->pos() + (event->pos() - event->oldPos()));
     }
 
     //When main window is opened, load shortcut settings
-    void showEvent(QShowEvent* event)
+    void showEvent(QShowEvent *event)
     {
         qRegisterMetaType<ShortCut>("ShortCut");
         qRegisterMetaTypeStreamOperators<ShortCut>("ShortCut");
